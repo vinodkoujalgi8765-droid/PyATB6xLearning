6b3bb57685093cc37d91352a42ff0f943574790e
vinodkoujalgi <vinodkoujalgi8765@gmail.com>
2025-10-29 21:00:04 +0530

    Task for 28-oct-2025 loop+condition+

--- src/Tasks/Task28-Oct-If_ELSE/10 down to 1.py
+++ src/Tasks/Task28-Oct-If_ELSE/10 down to 1.py
@@ -2,8 +2,11 @@
 Print numbers from 10 down to 1
 '''
 
-count = 10
+# count = 10
+#
+# while count > 0:
+#     print(count)
+#     count = count - 1
 
-while count > 0:
-    print(count)
-    count = count - 1
+for i in range(11,1,-1):
+    print(i)
