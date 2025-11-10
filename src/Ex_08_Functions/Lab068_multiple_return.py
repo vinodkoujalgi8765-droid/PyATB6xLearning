f6dcf55befaab48c5aa0fffef2e50850e40aebb1
vinodkoujalgi <vinodkoujalgi8765@gmail.com>
2025-11-10 19:23:35 +0530

    EX_09_Function_Scope_Pattern-04-Nov-2025

--- src/Ex_08_Functions/Lab068_multiple_return.py
+++ src/Ex_08_Functions/Lab068_multiple_return.py
@@ -1,8 +1,8 @@
 def fun_multiple_return(a,b):
     return a+b, a-b, a*b, a/b
 
-#print(fun_multiple_return(6,2))
+print(fun_multiple_return(6,2))
 result_sum,result_sub,result_mul,result_div = fun_multiple_return(6,2)
-print(result_sum)
-
- 
+print({result_sum,result_sub})
+total = fun_multiple_return(6,2)
+print(total)
