2ff2caebe5ad56930ae7a87f8f6bce5d53527404
vinodkoujalgi <vinodkoujalgi8765@gmail.com>
2025-11-28 15:22:02 +0530

    28-Nov_new_system

--- src/Ex_16Map_and_Filter/Lab_Filter3.py
+++ src/Ex_16Map_and_Filter/Lab_Filter3.py
@@ -4,7 +4,9 @@ def non_empty(name):
     return name != ""
 
 result = filter(non_empty, names)
-print(list(result))
+print(result)  #<filter object at 0x0000021AE8730700>
+print(list(result)) #['QA', 'Automation', 'Tester']
 
-result1 = map(non_empty, names)
-print(list(result1))
+
+# result1 = map(non_empty, names)
+# print(list(result1))
